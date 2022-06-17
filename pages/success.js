127b20b94dc25def48f900f964a747e38a02ef4d
thevictorwhyte <victordavidwhyte@gmail.com>
2022-06-17 14:01:38 +0100

    fix: typo in success.js

--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import Link from "next/lnk";
+import React, { useEffect } from "react";
+import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
 
 import { useStateContext } from "../context/StateContext";
